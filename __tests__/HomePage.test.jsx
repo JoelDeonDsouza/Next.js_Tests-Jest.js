b54c89ec86ff6a8912f6e8e441bec456dc380225
joeldeondsouza <jdeondsouza@gmail.com>
2023-06-29 20:58:45 +0200

    State states added

--- __tests__/HomePage.test.jsx
+++ __tests__/HomePage.test.jsx
@@ -1,27 +1,49 @@
 import { render, screen } from "@testing-library/react";
 // Render Component //
 import Home from "@/app/page";
+// test library //
+import userEvent  from "@testing-library/user-event";
 
 describe("Home Page - Renderings", () => {
-  // Text rendering function tests //
-  it("should render the home page h1 text", () => {
-    render(<Home />);
-    // screen.getByText("Hello World");
-    expect(screen.getByText("Hello World")).toBeInTheDocument();
+
+  //* Test that checks elements are rendered correctly *//
+  describe("Renderings", () => {
+    // Text rendering function tests //
+    it("should render the home page h1 text", () => {
+      render(<Home />);
+      // screen.getByText("Hello World");
+      expect(screen.getByText("Hello World")).toBeInTheDocument();
+    });
+    //  Buttons rendering function tests //
+    it("should render Button in the home page", () => {
+      render(<Home />);
+      // screen.getByRole("button");
+      expect(
+        screen.getByRole("button", { name: "Click here" })
+      ).toBeInTheDocument();
+    });
+    //  input function tests //
+    it("should render input in the home page", () => {
+      render(<Home />);
+      // expect(screen.getByRole("textbox")).toBeInTheDocument();
+      expect(screen.getByLabelText(/Email/)).toBeInTheDocument();
+    });
+
+    // state tests //
+    it("should renderelement when the state changes", () => {
+      render(<Home />);
+      expect(screen.queryByText("textbox")).not.toBeInTheDocument();
+    });
   });
-  //  Buttons rendering function tests //
-  it("should render Button in the home page", () => {
-    render(<Home />);
-    screen.getByRole("button");
-    expect(
-      screen.getByRole("button", { name: "Click here" })
-    ).toBeInTheDocument();
-  });
-  //  input function tests //
-  it("should render input in the home page", () => {
-    render(<Home />);
-    // expect(screen.getByRole("textbox")).toBeInTheDocument();
-    expect(screen.getByLabelText(/Email/)).toBeInTheDocument();
-  });
-  
+
+    //* Test that checks changes in state *//
+  describe('Behaviour', () => {
+     it('should render button when the state changes', async() => {
+      render(<Home />);
+      expect(screen.queryByText("Click here!")).not.toBeInTheDocument();
+      const displayBtn = screen.getByRole('button', {name:'Privacy*'});
+      await userEvent.click(displayBtn);
+      expect(screen.getByText("Click here!")).toBeInTheDocument();
+     });
+  })
 });
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,24 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
+  const [displayText, setDisplaytext] = useState();
   return (
     <main>
-       <h1>Hello World</h1>
-       <div>
-        <label htmlFor='email'>Email: </label>
-        <input id="email"/>
-       </div>
-       <div>
-        <label htmlFor='password'>Password: </label>
-        <input id="password"/>
-       </div>
-       <button>Click here</button>
+      <h1>Hello World</h1>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input id="email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input id="password" />
+      </div>
+      <button>Click here</button>
+      <div>
+        {displayText && <span>Click here!</span>}
+        <button onClick={() => setDisplaytext(!displayText)}>Privacy*</button>
+      </div>
     </main>
-  )
+  );
 }
